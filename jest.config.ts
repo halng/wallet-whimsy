3db86e00f0a36d8403735e7275506de420034987
haonguyentan2001 <haonguyentan2001@gmail.com>
2025-03-31 07:58:25 +0000

    feat: add github workflow

--- jest.config.ts
+++ jest.config.ts
@@ -121,7 +121,7 @@ const config: Config = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: ['default', ['jest-junit', { outputDirectory: '.reports', outputName: 'report.xml' }]],
 
   // Automatically reset mock state before every test
   resetMocks: true,
